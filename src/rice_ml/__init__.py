9f72ca251d0e1fe4164388f2576317eecaec414e
HawkVeluz <hv16@rice.edu>
2026-05-05 07:11:15 -0500

    removed unneeded preprocessing from scratch

--- src/rice_ml/__init__.py
+++ src/rice_ml/__init__.py
@@ -2,9 +2,7 @@ from .supervised_learning import (
     Perceptron, LinearRegression, LogisticRegression, MLP, KNN, KNNRegressor, 
     DecisionTree, DecisionTreeRegressor)
 
-from .preprocessing import train_test_split
 
 __all__ = [
     "Perceptron", "LinearRegression",  "LogisticRegression", "MLP", 
-    "KNN", "KNNRegressor", "DecisionTree", "DecisionTreeRegressor",
-    "train_test_split" ]
+    "KNN", "KNNRegressor", "DecisionTree", "DecisionTreeRegressor"]
